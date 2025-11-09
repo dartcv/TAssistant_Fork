643b561922c555be4b9e12f7ea4bda982f38365c
ReLimus <MoCui@proton.me>
2025-11-09 16:42:34 +0800

    feat(hook): HideQzoneVipTip

---
+++ app/src/main/java/io/live/timas/hook/items/qzone/HideQzoneVipTip.kt
@@ -0,0 +1,44 @@
+package io.live.timas.hook.items.qzone
+
+import android.content.Context
+import android.view.View
+import io.live.timas.annotations.RegisterToUI
+import io.live.timas.annotations.UiCategory
+import io.live.timas.hook.base.SwitchHook
+import top.sacz.xphelper.ext.toClass
+import top.sacz.xphelper.reflect.ConstructorUtils
+import top.sacz.xphelper.reflect.FieldUtils
+
+@RegisterToUI
+object HideQzoneVipTip : SwitchHook() {
+
+    override val name = "隐藏QQ空间VIP标识"
+
+    override val description =
+        "隐藏空间中头像下边的的\"开通VIP\", 会导致右侧小眼睛动画消失 (介意勿用)"
+
+    override val category = UiCategory.QZONE
+
+    override fun onHook(ctx: Context, loader: ClassLoader) {
+
+        val targetClass1 =
+            "com.qzone.reborn.feedx.widget.header.QZoneFeedxHeaderVipElement".toClass()
+        val targetClass2 = "com.qzone.reborn.feedx.widget.header.ax".toClass()
+
+        ConstructorUtils.create(targetClass1)
+            .paramTypes(View::class.java)
+            .hookAfter {
+                FieldUtils.create(thisObject)
+                    .fieldName("h")
+                    .setFirst(thisObject, null)
+            }
+
+        ConstructorUtils.create(targetClass2)
+            .paramTypes(View::class.java)
+            .hookAfter {
+                FieldUtils.create(thisObject)
+                    .fieldName("h")
+                    .setFirst(thisObject, null)
+            }
+    }
+}
