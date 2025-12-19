0aa12417ce1eedb5a507f6a3ec930cf402dee360
relimus <relimus@proton.me>
2025-12-19 14:47:05 +0800

    refactor: 转换类型方法变更 as T -> cast<T>()

--- app/src/main/kotlin/re/limus/timas/hook/items/message/core/Factory.kt
+++ app/src/main/kotlin/re/limus/timas/hook/items/message/core/Factory.kt
@@ -4,18 +4,18 @@ import re.limus.timas.hook.base.XBridge
 import re.limus.timas.hook.manager.HookManager
 import re.limus.timas.hook.items.message.PreventRevokeMsg
 import re.limus.timas.hook.utils.XLog
+import re.limus.timas.hook.utils.cast
 
-@Suppress("UNCHECKED_CAST")
 object Factory {
     /**
      * 获取Hook项实例
      * 优先从HookManager获取，如果没有则创建新实例
      */
-    fun <T : XBridge> getItem(clazz: Class<T>): T? {
+    inline fun <reified T : XBridge> getItem(clazz: Class<T>): T? {
         return when (clazz) {
             PreventRevokeMsg::class.java -> {
                 // 特殊处理 PreventRevokeMsg：直接返回单例实例
-                PreventRevokeMsg as T
+                PreventRevokeMsg.cast<T>()
             }
             else -> {
                 // 尝试从HookManager获取已注册的Hook实例
@@ -23,14 +23,14 @@ object Factory {
                     val hookItem = HookManager.getAllHooks().find { it.hook::class.java == clazz }
                     if (hookItem != null) {
                         // 找到匹配的Hook项，直接返回其hook实例
-                        hookItem.hook as T
+                        hookItem.hook.cast<T>()
                     } else {
                         // 如果Hook未在HookManager中注册，尝试直接创建实例
                         val newInstance = clazz.getDeclaredConstructor().newInstance()
-                        newInstance as T
+                        newInstance.cast<T>()
                     }
                 } catch (e: Exception) {
-                    XLog.e("Factory.getItem失败: ${clazz.name}\n $e")
+                    XLog.e("Factory.getItem失败", e)
                     null
                 }
             }
