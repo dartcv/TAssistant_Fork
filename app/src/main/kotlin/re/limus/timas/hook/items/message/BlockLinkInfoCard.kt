7ea6b85db7c2d650ec843832ecb9eb0611b4a44a
relimus <relimus@proton.me>
2025-12-31 17:25:47 +0800

    style: 代码样式变更 & 使用DexFinder
    使用DexFinder查找Constructor，写法更符合Kotlin

--- app/src/main/kotlin/re/limus/timas/hook/items/message/BlockLinkInfoCard.kt
+++ app/src/main/kotlin/re/limus/timas/hook/items/message/BlockLinkInfoCard.kt
@@ -8,16 +8,18 @@ import re.limus.timas.hook.base.SwitchHook
 // dartcv
 @RegisterToUI
 object BlockLinkInfoCard: SwitchHook() {
-    override val name: String
-        get() = "屏蔽链接信息卡片"
+    
+    override val name = "屏蔽链接信息卡片"
+    
     override val category = UiCategory.MESSAGE
 
     override fun onHook(ctx: Context, loader: ClassLoader) {
-        loader.loadClass("com.tencent.qqnt.kernel.nativeinterface.LinkInfo").declaredConstructors.forEach { constructor ->
-            constructor.hookBefore {
-                if(args[0]!=null) result = null
+        DexFinder.findMethod {
+            declaredClass = "com.tencent.qqnt.kernel.nativeinterface.LinkInfo".toClass()
+        }.hookConstructorBefore {
+            if (args[0] != null) { 
+                result = null
             }
         }
     }
-
 }
